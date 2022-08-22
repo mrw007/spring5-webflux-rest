7a08bb4e7088565a591f2628bfff6a00f74d53fb
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-22 10:42:49 +0100

    Initial Project Add

---
+++ src/test/java/mrw007/springframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package mrw007.springframework.spring5webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
