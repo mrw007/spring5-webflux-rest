96c9cf2b1b7aeb7d21b2ec160ef94bae1dc40c20
Wahib Kerkeni <mr.wahib@gmail.com>
2022-08-22 13:23:23 +0100

    Added Category Controller Unit Test

---
+++ src/test/java/mrw007/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,55 @@
+package mrw007.springframework.spring5webfluxrest.controllers;
+
+import mrw007.springframework.spring5webfluxrest.Repositories.CategoryRepository;
+import mrw007.springframework.spring5webfluxrest.models.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+class CategoryControllerTest {
+    public static final String CATEGORIES_BASE_URL = "/api/v1/categories/";
+
+    WebTestClient webTestClient;
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    void getAllCategories() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("cat1").build(),
+                        Category.builder().description("cat2").build()));
+
+        webTestClient.get()
+                .uri(CATEGORIES_BASE_URL)
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getCategoryById() {
+        BDDMockito.given(categoryRepository.findById("someID"))
+                .willReturn(Mono.just(Category.builder().description("cat").build()));
+
+        webTestClient.get()
+                .uri(CATEGORIES_BASE_URL + "someID")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
